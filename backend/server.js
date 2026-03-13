5936a313f6a90c47699d15c2a003672ad543dd4d
Vishrutha N K B <vishruthabalu06@gmail.com>
2026-03-14 01:28:55 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`🌸 Server running on port ${PORT}`));
